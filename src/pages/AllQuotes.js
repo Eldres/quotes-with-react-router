f36da512fa1b91f64e7e2f6bd494569b67a7be51
Josh Nagel <nageljp27@gmail.com>
2022-07-20 13:21:14 -0600

    added some dummy quote data, and fixed some import statements

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,23 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Josh",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Josh",
+    text: "Programming is fun!",
+  },
+];
+
 const AllQuotes = () => {
   return (
     <>
       <h1>All Quotes Page</h1>
+      <QuoteList quotes={DUMMY_QUOTES} />
     </>
   );
 };
