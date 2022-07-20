a5486523fbeb4c956349e36481a03f88cda5173e
Josh Nagel <nageljp27@gmail.com>
2022-07-20 13:08:40 -0600

    added a mainNav to avoid manual url editing. Added a layout wrapper component

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -7,7 +7,7 @@ const QuoteDetail = () => {
     <>
       <h1>Quote Detail Page</h1>
       <p>{params.quoteId}</p>
-      <Route path={`/quotes/${params.quoteId}/comments`} exact>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </>
